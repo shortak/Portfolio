f88303d23a4541732698f83eeaf1b5d347af4491
Sarthak Samal <152307069+shortak@users.noreply.github.com>
2024-10-16 22:16:40 -0600

    Added JS and updates

---
+++ script.js
@@ -0,0 +1,64 @@
+//We are not using elementbyID since that only works for one instace of that ID
+//Instead, use querySelectorAll and change things from there
+
+
+const dropdownContent = document.querySelectorAll(".dropdown-content");
+const button = document.querySelectorAll(".dropbtn");
+
+
+
+
+
+
+
+
+//To get the element that triggered the event, find the parent; the second child is the drop down menu; toggle the second child
+function toggleMenu(event){
+    const parentElement = event.target.parentElement;
+    const secondChild = parentElement.children[1]; 
+
+    secondChild.classList.toggle('show');
+}
+
+ 
+
+
+
+//Close menu functions
+
+//If the user hovers the menu, then off
+dropdownContent.forEach(function(element){  //for each instance of dropdownContent... 
+    element.addEventListener('mouseleave', function(){ //add event listener for mouse off...
+        element.classList.remove('show'); // which triggers the function to remove show
+    });
+});
+
+//If the user hovers off the button, call a function
+function closeMenu(event) { 
+
+    const parentElement = event.target.parentElement;
+    const secondChild = parentElement.children[1];
+    
+    if(!secondChild.matches(":hover")){
+        secondChild.classList.remove("show");
+    }
+   
+
+}
+
+
+// // Close the dropdown menu if the user moves outside of it
+// button.addEventListener("mouseover", function(event) {
+
+//     if (event.target == button) {
+//         console.log('yes');
+//       var dropdowns = dropdownContent;
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+// })
